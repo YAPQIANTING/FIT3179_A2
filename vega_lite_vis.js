2e40734524f0db3e50001e506ab047bc93951e63
YAPQIANTING <126408719+YAPQIANTING@users.noreply.github.com>
2024-10-06 19:58:00 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,10 @@ var vg_1 = "labour_force_choropleth_map.vg.json";
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
-    // (Refer to: https://vega.github.io/vega/docs/api/view/)
+}).catch(console.error);
 
+var vg_2 = "labour_force_bar_chart.vg.json";
+
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
+  // Access the Vega view instance as result.view
 }).catch(console.error);
