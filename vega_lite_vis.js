4534de6ebed47fe68f81971dbab9dcbbbfccb88e
YAPQIANTING <126408719+YAPQIANTING@users.noreply.github.com>
2024-10-15 23:24:29 +0800

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,28 +1,28 @@
-var vg_1 = "/visualisations/choropleth_map.vg.json";
+var vg_1 = "choropleth_map.vg.json";
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_2 = "/visualisations/stacked_bar_chart.vg.json";
+var vg_2 = "stacked_bar_chart.vg.json";
 
 vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_3 = "/visualisations/trellis_area_chart.vg.json";
+var vg_3 = "trellis_area_chart.vg.json";
 
 vegaEmbed("#trellis_area_chart", vg_3).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_4 = "/visualisations/bump_chart.vg.json";
+var vg_4 = "bump_chart.vg.json";
 
 vegaEmbed("#bump_chart", vg_4).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_5 = "/visualisations/bump_chart_2.vg.json";
+var vg_5 = "bump_chart_2.vg.json";
 
 vegaEmbed("#bump_chart2", vg_5).then(function(result) {
   // Access the Vega view instance as result.view
